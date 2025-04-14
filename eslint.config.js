83701923d4219cd095de80d3486858415f771528
yerimmseo <yerim7386@naver.com>
2025-04-14 22:48:57 +0900

    - prettier, eslint 설정 추가

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js, pluginPrettier },
+    extends: ['js/recommended'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    name: 'disable-eslint-style-rules-in-favor-of-prettier',
+    rules: {
+      ...configPrettier.rules,
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
