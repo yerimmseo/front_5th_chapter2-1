524d8ad577b22961dd46a2ae0aa32a0b98575a5a
서예림 <yerim7386@naver.com>
2025-04-16 11:00:48 +0900

    - 중복 코드 삭제

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -15,8 +15,6 @@ const initializeApp = () => {
   setupPromotions();
 };
 
-document.addEventListener('DOMContentLoaded', initializeApp);
-
 // 모듈이 로드되는 즉시 조건을 확인하고 실행
 // TEST 코드에서 element = null 인 경우가 있어서 추가함
 if (typeof window !== 'undefined' && document.getElementById('app')) {
