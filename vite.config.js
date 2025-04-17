f1c09e18126f9f0d80dca61d6734b97becf21595
yerimmseo <yerim7386@naver.com>
2025-04-18 04:50:03 +0900

    - 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/front_5th_chapter2-1/",
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "src/setupTests.js",
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.basic.html"), // `main`이 `index.html`로 출력됨
+      },
+    },
   },
 });
